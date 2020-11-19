8a904d26f50bfc0d5cd2e9dd19cba2484a35105e
Michael Cardona <mcardona9015@gmail.com>
2020-11-19 15:47:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,6 +11,10 @@ def introduction_with_language(name, language)
 end
 
 
+def introduction_with_language_optional(name, laguage="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
 
 
 
